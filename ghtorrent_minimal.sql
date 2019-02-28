74b90071e91b666673e0e93c2f1a328628d817cd
Sean Goggins <outdoors@acm.org>
2019-02-28 08:28:12 -0600

    added on repo owner

--- ghtorrent_minimal.sql
+++ ghtorrent_minimal.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b4ce3b9f4658accae43054946442796d42a22ed4b2e3487a0e2325ea4e84a9e
-size 75068117
+oid sha256:6fd07734eb284b748f8a606c04fe46bb0eba73504f4a375d0de881a8ec5d27fb
+size 75068283
